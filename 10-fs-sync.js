59056951acd468a21682792c8bf42da08aaf4579
Florence Ndemi <ndemiflorence@gmail.com>
2025-07-28 17:17:57 +0300

    Commit:Added consoles to async and sync files to see the differences

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -1,6 +1,5 @@
 const { readFileSync, writeFileSync } = require('fs')
-
-
+console.log('start')
 const first = readFileSync('./content/first.txt', 'utf8');
 const second = readFileSync('./content/second.txt', 'utf8');
 writeFileSync(
@@ -9,5 +8,6 @@ writeFileSync(
     `,
   { flag: 'a' }
 )
-
+console.log('done with this task')
+console.log('starting the next one')
 
--- 11-fs-async.js
+++ 11-fs-async.js
@@ -1,6 +1,6 @@
 const { readFile, writeFile } = require('fs')
 
-
+console.log('start')
 readFile('./content/first.txt', 'utf8', (err, result) => {
   if (err) {
     console.log(err)
@@ -12,8 +12,7 @@ readFile('./content/first.txt', 'utf8', (err, result) => {
       console.log(err)
       return
     }
-    const second = result;
-
+    const second = result
     writeFile('./content/result-async.txt', `
       Here is the result: ${first},${second}`,
       (err, result) => {
@@ -21,10 +20,11 @@ readFile('./content/first.txt', 'utf8', (err, result) => {
           console.log(err);
           return
         }
-        console.log(result)
+        console.log('done with this task')
       }
     )
   })
 })
+console.log('starting nxt task')
 
 
