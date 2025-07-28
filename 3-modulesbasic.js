6745202d5587a81b22f78a204556c5bd877fb1ab
Florence Ndemi <ndemiflorence@gmail.com>
2025-07-28 13:47:58 +0300

    Commit: Added introduction and basics for Node

---
+++ 3-modulesbasic.js
@@ -0,0 +1,16 @@
+//Modules-Encalpsulated Code (only share minimum)
+//CommonJs,every file is module by (default)
+const secret = 'SUPER SECRET'
+
+
+
+const john = 'john'
+const peter = 'peter'
+const sayHi = (name) => {
+  console.log(`Hello there ${name}`)
+
+
+}
+sayHi('susan')
+sayHi(john)
+sayHi(peter)
