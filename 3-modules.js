fe4247da3b958a1d2bb78dbb7cf48e205fa1d7aa
Florence Ndemi <ndemiflorence@gmail.com>
2025-07-28 14:35:10 +0300

    Commit:Added built in modules,i.e os,path

---
+++ 3-modules.js
@@ -0,0 +1,10 @@
+//Modules-Encalpsulated Code (only share minimum)
+//CommonJs,every file is module by (default)
+const { john, peter } = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavor')
+require('./7-mind-grenade')
+sayHi('susan')
+sayHi(john)
+sayHi(peter)
+
