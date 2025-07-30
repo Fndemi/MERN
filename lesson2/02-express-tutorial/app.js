a127f78e4040de59ea30a81aa0789917038f2012
Florence Ndemi <ndemiflorence@gmail.com>
2025-07-30 09:24:36 +0300

    Commit:served pages using http module

--- lesson2/02-express-tutorial/app.js
+++ lesson2/02-express-tutorial/app.js
@@ -1,15 +1,34 @@
 const http = require('http')
+const { readFileSync } = require('fs')
+
+//get all files
+const homepage = readFileSync('./navbar-app/index.html')
+const homeStyles = readFileSync('./navbar-app/styles.css')
+const homeImage = readFileSync('./navbar-app/logo.svg')
+const homeLogic = readFileSync('./navbar-app/browser-app.js')
+
 const server = http.createServer((req, res) => {
   //console.log((req.method))
-  const url = req.url;
+  const url = req.url
+  console.log(url)
   if (url === '/') {
     res.writeHead(200, { 'content-type': 'text/html' })
-    res.write('<h1>home page</h1>')
+    res.write(homepage)
     res.end()
   }
-  else if (url === '/about') {
-    res.writeHead(202, { 'content-type': 'text/html' })
-    res.write('<h1>About page</h1>')
+  else if (url === '/styles.css') {
+    res.writeHead(202, { 'content-type': 'text/css' })
+    res.write(homeStyles)
+    res.end()
+  }
+  else if (url === '/logo.svg') {
+    res.writeHead(202, { 'content-type': 'image/svg+xml' })
+    res.write(homeImage)
+    res.end()
+  }
+  else if (url === '/browser-app.js') {
+    res.writeHead(202, { 'content-type': 'text/javascript' })
+    res.write(homeLogic)
     res.end()
   }
   else {
