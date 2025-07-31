33e89d1d27ce6e5719ecdf5f8ca867f8071a77ab
Florence Ndemi <ndemiflorence@gmail.com>
2025-07-31 22:24:33 +0300

    Commit:Added methods for my routes

--- lesson2/02-express-tutorial/app.js
+++ lesson2/02-express-tutorial/app.js
@@ -2,13 +2,15 @@ const express = require('express')
 const app = express()
 
 let { people } = require('./data')
-
-
 //static assets
 app.use(express.static('./methods-public'))
 
+
 //parse form data
 app.use(express.urlencoded({ extended: false }))
+//parse json
+app.use(express.json())
+
 
 
 app.get('/api/people', (req, res) => {
@@ -16,13 +18,102 @@ app.get('/api/people', (req, res) => {
 })
 
 
-app.post('/login', (req, res) => {
-  res.send('POST')
+app.post('/api/people', (req, res) => {
+  const { name } = req.body
+  if (!name) {
+    return res.status(400).json({ success: false, msg: 'please provide name value' })
+  }
+  // res.status(201).json({})
+  res.status(201).send({ success: true, person: name })
 })
+
+app.post('/api/postman/people', (req, res) => {
+  const { name } = req.body
+  if (!name) {
+    return res.status(400).json({ success: true, person: name })
+  }
+  res.status(201).send({ success: true, data: [...people, name] })
+
+})
+
+
+
+
+
+
+app.post('/login', (req, res) => {
+  const { name } = req.body
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`)
+  }
+  res.status(401).send('Please provide Credentials')
+})
+
+
+
+app.put('/api/people/:id', (req, res) => {
+  const { id } = req.params  //xtracts the id from the url eg if /api/people/2 then id=2
+  const { name } = req.body
+
+
+  const person = people.find((person) => person.id === Number(id))
+
+  if (!person) {
+    return res.status(404).json({ success: false, msg: `No person with id ${id}` })
+  }
+
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name  // update the name
+    }
+    return person
+  })
+
+  res.status(200).json({ success: true, data: newPeople })
+})
+
+app.delete('/api/people/:id', (req, res) => {
+
+  const person = people.find((person) => person.id === Number(req.params.id))
+
+  if (!person) {
+    return res.
+      status(404)
+      .json({ success: false, msg: `No person with id ${req.params.id}` })
+  }
+
+  const newPeople = people.filter((person) => person.id !== Number(req.params.id));
+  return res.status(200).json({ success: true, data: newPeople })
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 app.listen(5000, () => {
   console.log('Server 5000 is starting ...')
 })
+
+
+
+
+
+
+
 
 
